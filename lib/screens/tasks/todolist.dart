9cb1ee21dd2200bf238ae2670d02a17de62b5791
SimonLab <simon.labondance@gmail.com>
2020-12-09 20:34:35 +0000

    save new task on sharedpreferences, #16

--- lib/screens/tasks/todolist.dart
+++ lib/screens/tasks/todolist.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:todolist/models/task.dart';
 import 'package:todolist/screens/tasks/task.dart';
 
@@ -10,15 +12,16 @@ class TodoList extends StatefulWidget {
 }
 
 class _TodoListState extends State<TodoList> {
-  final List<Task> _tasks = [];
+  List<Task> _tasks = [];
+
   final TextEditingController _controller = TextEditingController();
-  
+
   @override
   Widget build(BuildContext context) {
     return Column(children: [
       Expanded(
           child: ListView(
-            children: _tasks.map((Task task) => TaskWidget(task: task)).toList(),
+        children: _tasks.map((Task task) => TaskWidget(task: task)).toList(),
       )),
       TextField(
         controller: _controller,
@@ -28,20 +31,39 @@ class _TodoListState extends State<TodoList> {
             labelText: 'new task'),
         onSubmitted: (newTask) {
           setState(() {
-          _tasks.add(Task(text: newTask));
-            _controller.clear(); // clear the text input when an item is added to the list of tasks
+            _tasks.add(Task(text: newTask));
+            _controller
+                .clear(); // clear the text input when an item is added to the list of tasks
+            saveTasksToSharedPrefs(_tasks);
           });
         },
       )
     ]);
   }
 
+  Future<void> getTasksFromSharedPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    //prefs.clear();
+    final tasksJson = prefs.getString('tasks') ?? '[{"text": "hello", "completed": true}]';
+    // https://flutter.dev/docs/cookbook/networking/background-parsing#convert-the-response-into-a-list-of-photos
+    final jsonListTasks = jsonDecode(tasksJson).cast<Map<String, dynamic>>();
+
+    final tasks = jsonListTasks.map<Task>((m) => Task.fromJson(m)).toList();
+    setState(() {
+      _tasks = tasks;
+    });
+  }
+
+  Future<void> saveTasksToSharedPrefs(List<Task> tasks) async {
+    final prefs = await SharedPreferences.getInstance();
+    final json = jsonEncode(tasks);
+    prefs.setString('tasks', json);
+  }
+
   @override
   void initState() {
     // super.initState must be called first
     super.initState();
-    _tasks
-    ..add(Task(text: 'default task'))
-    ..add(Task(text: 'anotherstuff', completed: true));
+    getTasksFromSharedPrefs();
   }
 }
