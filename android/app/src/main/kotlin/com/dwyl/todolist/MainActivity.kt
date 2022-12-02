157eaab057ba2d0ad0e853b3de133a7faaea6f75
Luís Arteiro <lucho14garfield@gmail.com>
2022-12-02 19:57:29 +0000

    fix: Using new project.

--- android/app/src/main/kotlin/com/dwyl/todolist/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.dwyl.todolist
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/screens/tasks/providers.dart
+++
@@ -1,8 +0,0 @@
-// create a todolist provider which update when TodoListChangeNotifier calls notifyListeners()
-// when adding new task to the list
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:todolist/models/todoList.dart';
-
-final todolistProvider = ChangeNotifierProvider<TodoListChangeNotifier>((ref) {
-  return TodoListChangeNotifier();
-});
